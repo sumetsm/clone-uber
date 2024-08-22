9039f14e39d3810fa93e5b5cad4dd202c25b143b
sumetsm <sumetssr@gmail.com>
2024-08-23 04:19:12 +0700

    clean-code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
